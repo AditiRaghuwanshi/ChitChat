fb9fd160e55259c3db5df67ba97f91c473e4a968
Aditi Raghuwanshi <aditi.raghuwanshi430@nmims.edu.in>
2025-05-20 18:37:32 -0700

    cors

--- constants/config.js
+++ constants/config.js
@@ -1,14 +1,28 @@
-const corsOptions = {
+// const corsOptions = {
    
-       origin: [
-           "http://localhost:5173",
-           // "http://localhost:4173",
-           process.env.CLIENT_URL,
-       ],
-       credentials: true,
+//        origin: [
+//            "http://localhost:5173",
+//            // "http://localhost:4173",
+//            process.env.CLIENT_URL,
+//        ],
+//        credentials: true,
    
+// };
+
+// const CHITCHAT_TOKEN = "chitchat-token";
+
+
+// export { corsOptions, CHITCHAT_TOKEN };
+
+// config.js
+export const corsOptions = {
+  origin: ["https://chit-chat-frontend-sooty.vercel.app"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
-const CHITCHAT_TOKEN = "chitchat-token";
 
-export { corsOptions, CHITCHAT_TOKEN };
+
+
+

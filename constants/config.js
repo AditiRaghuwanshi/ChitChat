cbfb01ee20594c9fb72d3224723d6267a1a82719
Aditi Raghuwanshi <aditi.raghuwanshi430@nmims.edu.in>
2025-05-22 15:17:54 -0700

    responsivness done

--- constants/config.js
+++ constants/config.js
@@ -1,30 +1,30 @@
-// const corsOptions = {
+const corsOptions = {
    
-//        origin: [
-//         // "https://chit-chat-frontend-sooty.vercel.app",
-//            "http://localhost:5173",
-//            // "http://localhost:4173",
-//         //    process.env.CLIENT_URL,
-//        ],
-//        credentials: true,
-//         allowedHeaders: ["Content-Type", "Authorization"],
+       origin: [
+        // "https://chit-chat-frontend-sooty.vercel.app",
+           "http://localhost:5173",
+           // "http://localhost:4173",
+        //    process.env.CLIENT_URL,
+       ],
+       credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
    
-// };
-
-// const CHITCHAT_TOKEN = "chitchat-token";
-
-
-// export { corsOptions, CHITCHAT_TOKEN };
-
-export const corsOptions = {
-  origin: [
-    "https://chit-chat-frontend-dtf5.vercel.app",
-  ],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 
-export const CHITCHAT_TOKEN = "chitchat-token";
+const CHITCHAT_TOKEN = "chitchat-token";
+
+
+export { corsOptions, CHITCHAT_TOKEN };
+
+// export const corsOptions = {
+//   origin: [
+//     "https://chit-chat-frontend-dtf5.vercel.app",
+//   ],
+//   credentials: true,
+//   allowedHeaders: ["Content-Type", "Authorization"],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// };
+
+// export const CHITCHAT_TOKEN = "chitchat-token";
 
 
